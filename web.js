0f16895dce9fe96b39b64f90b91746a12d448523
Jonathan Owers <jonathan.owers@gmail.com>
2013-07-06 12:32:58 +0000

    Change to read output from index.html

--- web.js
+++ web.js
@@ -3,7 +3,8 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var html_file = fs.readFileSync('index.html');
+  response.send(html_file.toString());
 });
 
 var port = process.env.PORT || 5000;
